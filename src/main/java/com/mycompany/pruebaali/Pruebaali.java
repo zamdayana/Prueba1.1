23ea700777e6ff224b9c6979016ef190cf959c86
zamdayana <alissonzambrano88@gmail.com>
2024-11-25 15:45:05 -0500

    PRUEBA SSH

---
+++ src/main/java/com/mycompany/pruebaali/Pruebaali.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebaali;
+
+/**
+ *
+ * @author aliss
+ */
+public class Pruebaali {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("PRUEBA ");
+    }
+}
